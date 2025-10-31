835b811339940cb16393312af484e59cddae2743
cwassointt <u202112078@upc.edu.pe>
2025-10-31 01:40:23 -0500

    feat: add GetUserNotificationsQuery for retrieving user-specific notifications

---
+++ src/main/java/com/nrgserver/ergovision/notifications/domain/model/queries/GetUserNotificationsQuery.java
@@ -0,0 +1,6 @@
+package com.nrgserver.ergovision.notifications.domain.model.queries;
+
+import com.nrgserver.ergovision.notifications.domain.model.valueobjects.NotificationStatus;
+import com.nrgserver.ergovision.notifications.domain.model.valueobjects.NotificationType;
+
+public record GetUserNotificationsQuery(Long userId, NotificationStatus statusFilter, NotificationType typeFilter) {}
