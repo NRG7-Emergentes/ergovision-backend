c879e2e0897138c725fac79e5b4038cba8ee6665
cwassointt <u202112078@upc.edu.pe>
2025-11-04 23:49:42 -0500

    feat: remove in-memory UserPreferencesRepository for development/testing

--- src/main/java/com/nrgserver/ergovision/notifications/domain/exceptions/InvalidNotificationException.java
+++
@@ -1,5 +0,0 @@
-package com.nrgserver.ergovision.notifications.domain.exceptions;
-
-public class InvalidNotificationException extends RuntimeException {
-    public InvalidNotificationException(String message) { super(message); }
-}
