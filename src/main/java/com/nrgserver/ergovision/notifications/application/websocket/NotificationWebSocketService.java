d2c1be6d83a4e24da1b4ac603ba5bf0c20afadf0
cwassointt <u202112078@upc.edu.pe>
2025-11-07 23:21:33 -0500

    feat: add NotificationWebSocketService for handling WebSocket notifications

---
+++ src/main/java/com/nrgserver/ergovision/notifications/application/websocket/NotificationWebSocketService.java
@@ -0,0 +1,26 @@
+package com.nrgserver.ergovision.notifications.application.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationWebSocketService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public String processIncomingMessage(String message) {
+        return "Echo: " + message;
+    }
+
+    // Notifica a todos los suscriptores (broadcast)
+    public void broadcastNotification(Object payload) {
+        messagingTemplate.convertAndSend("/topic/all", payload);
+    }
+
+    // Envía una notificación a un usuario específico
+    public void sendPrivateMessage(String userId, Object payload) {
+        messagingTemplate.convertAndSendToUser(userId, "/queue/private", payload);
+    }
+}
