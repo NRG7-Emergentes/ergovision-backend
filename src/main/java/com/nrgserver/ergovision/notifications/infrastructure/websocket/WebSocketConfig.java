76db0db86563245aba7c785d0a8cf10c886ae0ac
cwassointt <u202112078@upc.edu.pe>
2025-11-07 23:21:56 -0500

    feat: add WebSocket configuration for message handling

---
+++ src/main/java/com/nrgserver/ergovision/notifications/infrastructure/websocket/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.nrgserver.ergovision.notifications.infrastructure.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws") // endpoint base
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // soporte para clientes que no usan WebSocket nativo
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app"); // prefijo para mensajes entrantes
+        registry.enableSimpleBroker("/topic", "/queue"); // destinos de salida
+        registry.setUserDestinationPrefix("/user"); // para mensajes privados
+    }
+}
