03a3398e8586c2d9521b7f5cfdff2a88b9197d02
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-11-09 12:39:13 -0500

    feat: add create monitoring session resource.

---
+++ src/main/java/com/nrgserver/ergovision/monitoring/interfaces/rest/resources/CreateMonitoringSessionResource.java
@@ -0,0 +1,17 @@
+package com.nrgserver.ergovision.monitoring.interfaces.rest.resources;
+
+import java.time.Instant;
+
+public record CreateMonitoringSessionResource (
+        String startDate,
+        String endDate,
+        Double score,
+        Double goodScore,
+        Double badScore,
+        Double goodPostureTime,
+        Double badPostureTime,
+        Double duration,
+        Integer numberOfPauses,
+        Double averagePauseDuration
+){
+}
