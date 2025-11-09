6b96478c2131fda263e467bd6cf79ed32ceaa009
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-11-09 12:38:32 -0500

    feat: add monitoring session query implement.

--- src/main/java/com/nrgserver/ergovision/monitoring/aplication/internal/queryservices/MonitoringSessionQueryImpl.java
+++ src/main/java/com/nrgserver/ergovision/monitoring/aplication/internal/queryservices/MonitoringSessionQueryImpl.java
@@ -1,4 +1,24 @@
 package com.nrgserver.ergovision.monitoring.aplication.internal.queryservices;
 
-public class MonitoringSessionQueryImpl {
+import com.nrgserver.ergovision.monitoring.domain.model.aggregates.MonitoringSession;
+import com.nrgserver.ergovision.monitoring.domain.model.queries.GetMonitoringSessionByIdQuery;
+import com.nrgserver.ergovision.monitoring.domain.services.MonitoringSessionQueryService;
+import com.nrgserver.ergovision.monitoring.infrastructure.persistence.jpa.repositories.MonitoringSessionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MonitoringSessionQueryImpl implements MonitoringSessionQueryService {
+
+    private final MonitoringSessionRepository monitoringSessionRepository;
+
+    public MonitoringSessionQueryImpl(MonitoringSessionRepository monitoringSessionRepository) {
+        this.monitoringSessionRepository = monitoringSessionRepository;
+    }
+
+    @Override
+    public Optional<MonitoringSession> handle(GetMonitoringSessionByIdQuery query) {
+        return this.monitoringSessionRepository.findById(query.monitoringSessionId());
+    }
 }
