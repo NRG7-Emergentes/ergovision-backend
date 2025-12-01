294cd8467d39b09e4157e14d22e318211a05b304
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-12-01 16:40:56 -0500

    feat: add handler for deleting monthly progress by statistics ID
    Introduces a new method to handle DeleteMonthlyProgressByStatisticsIdCommand in MonthlyProgressCommandService. This allows deletion of monthly progress records based on statistics ID.

--- src/main/java/com/nrgserver/ergovision/statistics/domain/services/MonthlyProgressCommandService.java
+++ src/main/java/com/nrgserver/ergovision/statistics/domain/services/MonthlyProgressCommandService.java
@@ -1,9 +1,6 @@
 package com.nrgserver.ergovision.statistics.domain.services;
 
-import com.nrgserver.ergovision.statistics.domain.model.commands.CreateMonthlyProgressCommand;
-import com.nrgserver.ergovision.statistics.domain.model.commands.CreateOnlyMonthlyProgressCommand;
-import com.nrgserver.ergovision.statistics.domain.model.commands.DeleteMonthlyProgressCommand;
-import com.nrgserver.ergovision.statistics.domain.model.commands.UpdateMonthlyProgressCommand;
+import com.nrgserver.ergovision.statistics.domain.model.commands.*;
 import com.nrgserver.ergovision.statistics.domain.model.entities.MonthlyProgress;
 
 import java.util.Optional;
@@ -13,4 +10,5 @@ public interface MonthlyProgressCommandService {
     Long handle(CreateMonthlyProgressCommand command);
     Optional<MonthlyProgress> handle(UpdateMonthlyProgressCommand command);
     void handle(DeleteMonthlyProgressCommand command);
+    void handle(DeleteMonthlyProgressByStatisticsIdCommand command);
 }
